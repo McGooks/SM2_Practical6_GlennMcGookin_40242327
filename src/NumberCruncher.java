274684de8eb4814a8d46b454d0d6c27c32d2284c
Glenn McGookin <glennyboi1@me.com>
2019-02-27 18:01:19 +0000

    Initial commit

---
+++ src/NumberCruncher.java
@@ -0,0 +1,24 @@
+public class NumberCruncher {
+
+    /**
+     * Method adds two numbers and returns the result
+     * @param num1
+     * @param num2
+     * @return
+     */
+
+    public int addNumbers(int num1, int num2){
+        return num1+num2;
+    }
+
+    /**
+     * Method adds three numbers and returns the result
+     * @param num1
+     * @param num2
+     * @param num3
+     */
+
+    public int addNumbers(int num1, int num2,int num3){
+        return num1+num2+num3;
+    }
+}

2ea59860261b3822fd3fec906f71ed010bdc203c
Glenn McGookin <glenn.mcgookin@icloud.com>
2019-02-28 18:54:36 +0000

    commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,7 @@
 public class Employee {
 
     private String name;
+    private int age;
 
     public String getName() {
         return name;
@@ -10,4 +11,16 @@ public class Employee {
         this.name = name;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
